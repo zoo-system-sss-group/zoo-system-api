ed0e0a3bb4da00f51e599975693c05c0aa05c2b4
viliam0206 <v.trclam@gmail.com>
2023-10-25 16:29:58 +0700

    [lam][fix area dto bug]

--- ZooManagementWebApi/Controllers/AreasController.cs
+++ ZooManagementWebApi/Controllers/AreasController.cs
@@ -9,101 +9,100 @@ using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Results;
 using ZooManagementWebApi.DTOs;
 
-namespace ZooManagementWebApi.Controllers
+namespace ZooManagementWebApi.Controllers;
+
+[EnableQuery]
+[Authorize]
+public class AreasController : ControllerBase
 {
-    [EnableQuery]
-    [Authorize]
-    public class AreasController : ControllerBase
+    private readonly IAreaRepository _areaRepository;
+    private readonly IMapper mapper;
+    public AreasController(IAreaRepository areaRepository, IMapper mapper)
     {
-        private readonly IAreaRepository _areaRepository;
-        private readonly IMapper mapper;
-        public AreasController(IAreaRepository areaRepository, IMapper mapper)
+        _areaRepository = areaRepository;
+        this.mapper = mapper;
+    }
+    [HttpGet]
+    public ActionResult<IQueryable<Area>> Get()
+    {
+        IQueryable<Area> areas;
+        try
         {
-            _areaRepository = areaRepository;
-            this.mapper = mapper;
+            areas = _areaRepository.GetAreasAsync();
         }
-        [HttpGet]
-        public ActionResult<IQueryable<Area>> Get()
+        catch (Exception ex)
         {
-            IQueryable<Area> areas;
-            try
-            {
-                areas = _areaRepository.GetAreasAsync();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            return Ok(areas);
+            return BadRequest(ex.Message);
         }
-        [HttpGet]
-        public ActionResult<SingleResult> Get([FromRoute] int key)
-        {
-            var area = _areaRepository.GetAreaByIdAsync(key);
+        return Ok(areas);
+    }
+    [HttpGet]
+    public ActionResult<SingleResult> Get([FromRoute] int key)
+    {
+        var area = _areaRepository.GetAreaByIdAsync(key);
 
-            if (area == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(new SingleResult<Area>(area));
-        }
-        [HttpPost]
-        [Authorize(Roles = "Staff")]
-        public async Task<ActionResult<Area>> Post([FromBody] AreaDto dto)
+        if (area == null)
         {
-            Area area;
-            try
-            {
-                area = mapper.Map<Area>(dto);
-                await _areaRepository.AddAreaAsync(area);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            return CreatedAtAction("Get", new { key = area.Id }, area);
-        }
-        [HttpPut]
-        [Authorize(Roles = "Staff")]
-        public async Task<IActionResult> Put([FromRoute] int key, [FromBody] AreaDto dto)
-        {
-            try
-            {
-                var area = mapper.Map<Area>(dto);
-                area.Id = key;
-                await _areaRepository.UpdateAreaAsync(area);
-            }
-            catch (ArgumentException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            return NoContent();
+            return NotFound();
         }
 
-        [HttpDelete]
-        [Authorize(Roles = "Staff")]
-        public async Task<IActionResult> Delete([FromRoute] int key)
+        return Ok(new SingleResult<Area>(area));
+    }
+    [HttpPost]
+    [Authorize(Roles = "Staff")]
+    public async Task<ActionResult<Area>> Post([FromBody] AreaDto dto)
+    {
+        Area area;
+        try
         {
-            try
-            {
-                await _areaRepository.SoftDeleteAreaAsync(key);
-            }
-            catch (ArgumentException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            return NoContent();
+            area = mapper.Map<Area>(dto);
+            await _areaRepository.AddAreaAsync(area);
         }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+        return CreatedAtAction("Get", new { key = area.Id }, area);
+    }
+    [HttpPut]
+    [Authorize(Roles = "Staff")]
+    public async Task<IActionResult> Put([FromRoute] int key, [FromBody] AreaDto dto)
+    {
+        try
+        {
+            var area = mapper.Map<Area>(dto);
+            area.Id = key;
+            await _areaRepository.UpdateAreaAsync(area);
+        }
+        catch (ArgumentException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = "Staff")]
+    public async Task<IActionResult> Delete([FromRoute] int key)
+    {
+        try
+        {
+            await _areaRepository.SoftDeleteAreaAsync(key);
+        }
+        catch (ArgumentException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+        return NoContent();
     }
 }
--- ZooManagementWebApi/DTOs/AreaDto.cs
+++ ZooManagementWebApi/DTOs/AreaDto.cs
@@ -2,9 +2,10 @@
 {
     public class AreaDto
     {
+        public string Code { get; set; } = default!; // A,B,C
         public string Name { get; set; } = default!;
         public string Location { get; set; } = default!;
-        public string? Description { get; set; }
+        public string Description { get; set; } = default!;
         public int Capacity { get; set; }
     }
 }
