b7f370446cb0c827966458608f31fd37bd4cdac2
Đỗ Thành Bộ <112932777+dothanhbo@users.noreply.github.com>
2023-11-04 00:07:27 +0700

    [Dothanhbo][fix feeding] (#31)
    * [dothanhbo][add odata of my parts]
    
    * [dothanhbo][fix odata]
    
    * [dothanhbo][add feeding history]
    
    * [dothanhbo][fix conflig2]
    
    * [dothanhbo][add update cage]
    
    * [dothanhbo][add annotation]
    
    * [dothanhbo][fix feeding]
    
    * [fix }]
    
    * [dothanhbo][add some annotation]
    
    * [dothanhbo][fix feeding]

--- ZooManagementWebApi/DTOs/CageDto.cs
+++ ZooManagementWebApi/DTOs/CageDto.cs
@@ -1,11 +1,17 @@
-﻿namespace ZooManagementWebApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZooManagementWebApi.DTOs
 {
     public class CageDto
     {
+        [Required(ErrorMessage = "Code is required!")]
         public string Code { get; set; } = default!;
+        [Required(ErrorMessage = "Cage name is required!")]
         public string Name { get; set; } = default!;
+        [Required(ErrorMessage = "Location is required!")]
         public string Location { get; set; } = default!;
         public string? Image { get; set; }
+        [Required(ErrorMessage = "Capacity is required!")]
         public int Capacity { get; set; }
         public string? Description { get; set; }
         public int AreaId { get; set; }
