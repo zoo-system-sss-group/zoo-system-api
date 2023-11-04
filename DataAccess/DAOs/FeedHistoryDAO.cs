cd6fe84811024cb60ec2a2dc367c8387df88793e
dothanhbo <112932777+dothanhbo@users.noreply.github.com>
2023-11-04 23:02:02 +0700

    [dothanhbo][hot fix]

--- DataAccess/DAOs/FeedHistoryDAO.cs
+++ DataAccess/DAOs/FeedHistoryDAO.cs
@@ -16,7 +16,7 @@ namespace DataAccess.DAOs
         {
             List<FeedHistory> diets;
             using (var context = new AppDBContext(_configuration))
-                diets = await context.FeedHistories.Where(x => x.AnimalId == animalId & x.FeedingDate == DateTime.Today).ToListAsync();
+                diets = await context.FeedHistories.Where(x => x.AnimalId == animalId & x.FeedingDate.Date == DateTime.Today.Date).ToListAsync();
             return diets;
         }
         public IQueryable<FeedHistory> GetFeedHistories()
@@ -24,7 +24,7 @@ namespace DataAccess.DAOs
             IQueryable<FeedHistory> diets;
             var context = new AppDBContext(_configuration);
             diets = context.FeedHistories.OrderByDescending(x => x.FeedingDate);
-                diets = await context.FeedHistories.Where(x => x.AnimalId == animalId).ToListAsync();
+                diets = context.FeedHistories;
             return diets;
         }
     }
